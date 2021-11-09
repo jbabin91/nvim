94fbc8a09df161fdb0700436e0b12b5f59544258
Jace Babin <jbabin91@gmail.com>
2021-11-08 23:08:56 -0700

    WIP

--- init.lua
+++ init.lua
@@ -1,1 +1,12 @@
+local modules = {
+}
 
+for _, mod in ipairs(modules) do
+  local ok, err = pcall(require, mod)
+  if not ok then
+    vim.notify(
+      string.format("--- Module '%s' failed to load due to error: %s", mod, err),
+      vim.log.levels.ERROR
+    )
+  end
+end
