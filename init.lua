445d1124ac3352e98de7a334f1db84253a87db3b
Jace Babin <jbabin91@gmail.com>
2021-11-10 19:03:04 -0700

    Starting over with a new personal config

--- init.lua
+++ init.lua
@@ -1,16 +1,4 @@
-require("core")
-
-local user_config = CONFIG_PATH .. "/lua/core/user-config/init.lua"
-
-if not JB.utils.file.exists(user_config) then JB.utils.file.create(user_config) end
-
--- User config that overrides the above
-vim.cmd("luafile " .. user_config)
-
 local modules = {
-  "core.options",
-  "core.plugins",
-  "core.keybindings",
 }
 
 for _, mod in ipairs(modules) do
@@ -21,24 +9,4 @@ for _, mod in ipairs(modules) do
       vim.log.levels.ERROR
     )
   end
-end
--- Syntax and Visual
--- If a custom theme is wanted, require() that in user-config.init
--- Otherwise if the default theme is not wanted change JB.settings.colorscheme
--- This will return nil if it's not found
-if JB.settings.colorscheme ~= "custom" then
-  JB.utils.plugins.require("configs.colorscheme." .. JB.settings.colorscheme)
-end
-
--- LSP and Autocomplete
--- require("configs.language-servers")
-
--- Git
-require("configs.gitsigns")
-
--- Other
-require("configs.which-key")
-
-if JB.settings.transparent_bg then
-  vim.cmd([[hi Normal guibg=NONE ctermbg=NONE]])
 end
