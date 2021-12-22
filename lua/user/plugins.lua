aacbf87ee5e0d996823ab46bb2e6a87faa3d1bca
Jace Babin <jbabin91@gmail.com>
2021-12-22 01:30:23 -0600

    add indentline

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -55,6 +55,7 @@ return packer.startup(function(use)
   use "akinsho/toggleterm.nvim"
   use "ahmedkhalf/project.nvim"
   use "lewis6991/impatient.nvim"
+  use "lukas-reineke/indent-blankline.nvim"
 
   -- Nvim Profiling
   use "dstein64/vim-startuptime"
