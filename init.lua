eeb6eba82805d608f1f03b94bc93945eb11b5393
Jace Babin <jbabin91@gmail.com>
2021-12-30 21:08:18 -0700

    Working on configuration focused on speed

--- init.lua
+++ init.lua
@@ -1,25 +1,20 @@
-require "user.impatient"
-require "user.options"
-require "user.keymaps"
-require "user.plugins"
--- require "user.colorscheme"
-require("user.colors." .. "kanagawa")
-require "user.lsp"
-require "user.configs"
+-- Load basic configuration
+local utils_ok, utils = pcall(require, "utils")
+if not utils_ok then return end
 
--- require "user.cmp"
--- require "user.telescope"
--- require "user.treesitter"
--- require "user.autopairs"
--- require "user.comment"
--- require "user.gitsigns"
--- require "user.nvim-tree"
--- require "user.bufferline"
--- require "user.lualine"
--- require "user.toggleterm"
--- require "user.project"
--- require "user.indentline"
--- require "user.alpha"
--- require "user.whichkey"
--- require "user.autocommands"
--- require "user.configs"
+local modules = {
+  "options",
+  "keymap",
+  "commands",
+}
+
+for _, module_name in ipairs(modules) do
+  local ok, err = pcall(require, module_name)
+  if not ok then
+    local msg = "calling module: " .. module_name .. " fail: " .. err
+    utils.log_err(msg)
+  end
+end
+
+-- load plugins
+utils.load_plugins()
