9b92f709d8f8c1a347cddd98f36086a80a9cd385
Jace Babin <jbabin91@gmail.com>
2022-03-08 18:13:15 -0700

    Working on a minimal config
    Signed-off-by: Jace Babin <jbabin91@gmail.com>

---
+++ init.lua
@@ -0,0 +1,23 @@
+local disabled_builtins = {
+  "2html_plugin",
+  "fzf",
+  "getscript",
+  "getscriptPlugin",
+  "gzip",
+  "logipat",
+  "netrw",
+  "netrwPlugin",
+  "netrwSettings",
+  "netrwFileHandlers",
+  "tar",
+  "tarPlugin",
+  "rrhelper",
+  "vimball",
+  "vimballPlugin",
+  "zip",
+  "zipPlugin",
+}
+
+for _, plugin in pairs(disabled_builtins) do
+  vim.g["loaded_" .. plugin] = 1
+end
