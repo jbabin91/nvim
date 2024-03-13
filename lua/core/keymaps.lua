d8509137ab28fcf5469b459f4c81a8e53279dabc
Jace Babin <jbabin91@gmail.com>
2024-03-13 08:59:14 -0700

    setup lazy plugin manager and default options

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,17 @@
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line up" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line down" })
+
+vim.keymap.set("n", "<C-s>", ":w <CR>", { desc = "Save current buffer" })
+
+vim.keymap.set("n", "n", "nzz", { desc = "Goto next search centered" })
+vim.keymap.set("n", "N", "Nzz", { desc = "Goto prev search centered" })
+
+vim.keymap.set("n", "m", "q", { desc = "Start macro" })
+vim.keymap.set("n", "q", "<Nop>", { desc = "Clear previous macro keymap entry" })
+
+vim.g.fzf_action = {
+  ["ctrl-s"] = "split",
+  ["ctrl-v"] = "vsplit",
+}
+
+vim.keymap.set("v", "<leader>y", "\"+y", { desc = "Copy to clipboard" })
